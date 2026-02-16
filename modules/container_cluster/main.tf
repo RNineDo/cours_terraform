1f92f6ee27f2044377670cff0cd9b1b2d4f02f15
rninedo <dorian.saintmartin@outlook.fr>
2026-02-16 12:28:47 +0100

    Ajout validation et personnalisation index.html

--- modules/container_cluster/main.tf
+++ modules/container_cluster/main.tf
@@ -12,9 +12,7 @@ resource "docker_image" "app_image" {
 
 resource "docker_container" "app_container" {
   count = var.container_count
-
-  # Génère des noms dynamiques : my-app-0, my-app-1...
-  name  = "my-app-${count.index}"
+  name  = "node-${count.index}"
   image = docker_image.app_image.image_id
   
   memory     = var.container_memory
@@ -22,7 +20,20 @@ resource "docker_container" "app_container" {
 
   ports {
     internal = 80
-    # Calcul dynamique du port : 3000, 3001, 3002...
     external = var.starting_port + count.index
+  }
+
+  # Injection de la page d'accueil personnalisée
+  upload {
+    content = <<EOF
+<html>
+  <head><title>Terraform Lab</title></head>
+  <body>
+    <h1>Bienvenue sur le conteneur : node-${count.index}</h1>
+    <p>Ce serveur Nginx a été déployé via Terraform.</p>
+  </body>
+</html>
+EOF
+    file    = "/usr/share/nginx/html/index.html"
   }
 }
--- modules/container_cluster/variables.tf
+++ modules/container_cluster/variables.tf
@@ -1,24 +1,36 @@
 variable "image_name" {
-  description = "Nom de l'image Docker à utiliser"
+  description = "Nom de l'image Docker"
   type        = string
 }
 
 variable "container_memory" {
-  description = "Limite de mémoire pour chaque conteneur (en MB)"
+  description = "Mémoire en MB (Min 64MB)"
   type        = number
+  validation {
+    condition     = var.container_memory >= 64
+    error_message = "La mémoire allouée doit être d'au moins 64 MB."
+  }
 }
 
 variable "is_privileged" {
-  description = "Définit si le conteneur doit être en mode privilégié"
+  description = "Mode privilégié"
   type        = bool
 }
 
 variable "container_count" {
-  description = "Nombre de conteneurs à générer"
+  description = "Nombre de conteneurs (1 à 10)"
   type        = number
+  validation {
+    condition     = var.container_count > 0 && var.container_count <= 10
+    error_message = "Vous devez créer entre 1 et 10 conteneurs."
+  }
 }
 
 variable "starting_port" {
-  description = "Le port externe du premier conteneur (ex: 3000)"
+  description = "Port de départ (1024-65535)"
   type        = number
+  validation {
+    condition     = var.starting_port >= 1024 && var.starting_port <= 65535
+    error_message = "Le port doit être compris entre 1024 et 65535 (ports non-privilégiés)."
+  }
 }
